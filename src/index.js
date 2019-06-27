02bcba9285f80303c69631f2025631d8f533d91e
Luiz Victor Linhares Rocha <luizvictorlrocha@gmail.com>
2019-06-27 17:56:50 -0300

    configures dotenv

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './config/dotenv';
 import './config/reactotron';
 import './index.css';
 import App from './App';
+import 'react-activity/dist/react-activity.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
