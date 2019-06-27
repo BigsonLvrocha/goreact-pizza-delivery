628b988b099bda594472dc9afe29f987b5d9a1c8
Luiz Victor Linhares Rocha <luizvictorlrocha@gmail.com>
2019-06-27 10:33:56 -0300

    adds input state tologin component

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,19 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PizzaLayout from '../../components/Layouts/Pizza';
 import Logo from '../../Assets/logo@3x.png';
 import { Container } from './styles';
 
-const Login = () => (
-  <PizzaLayout>
-    <Container>
-      <img src={Logo} alt="pizza logo" />
-      <form>
-        <input placeholder="Seu e-mail" type="email" />
-        <input placeholder="Senha secreta" type="password" />
-        <button type="submit">Entrar</button>
-      </form>
-    </Container>
-  </PizzaLayout>
-);
+class Login extends Component {
+  state = {
+    email: '',
+    password: '',
+  };
+
+  render() {
+    const { email, password } = this.state;
+    return (
+      <PizzaLayout>
+        <Container>
+          <img src={Logo} alt="pizza logo" />
+          <form>
+            <input
+              value={email}
+              onChange={event => this.setState({ email: event.target.value })}
+              placeholder="Seu e-mail"
+              type="email"
+            />
+            <input
+              value={password}
+              onChange={event => this.setState({ password: event.target.value })}
+              placeholder="Senha secreta"
+              type="password"
+            />
+            <button type="submit" disabled={!email || !password}>
+              Entrar
+            </button>
+          </form>
+        </Container>
+      </PizzaLayout>
+    );
+  }
+}
 
 export default Login;
