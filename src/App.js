f7799e5d86173981baf50e686faf43f6405ffdc8
Luiz Victor Linhares Rocha <luizvictorlrocha@gmail.com>
2019-06-28 01:12:29 -0300

    adds initial check in root app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import Router from './routes';
+import { Creators as SessionActions } from './store/ducks/session';
 import store from './store';
 
+store.dispatch(SessionActions.fetchLoginStorage());
 function App() {
   return (
     <Provider store={store} className="App">
