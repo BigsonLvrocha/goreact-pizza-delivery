728638c5d61f4324c0b3fc3193ee2ae80851036c
Luiz Victor Linhares Rocha <luizvictorlrocha@gmail.com>
2019-06-28 01:12:00 -0300

    adds saga to root store

--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -1,7 +1,10 @@
 import { all, takeLatest } from 'redux-saga/effects';
-import { loginRequest } from './session';
+import { loginRequest, fetchloginStorage } from './session';
 import { Types as SesionTypes } from '../ducks/session';
 
 export default function* rootSaga() {
-  yield all([takeLatest(SesionTypes.LOGIN_REQUEST, loginRequest)]);
+  yield all([
+    takeLatest(SesionTypes.LOGIN_REQUEST, loginRequest),
+    takeLatest(SesionTypes.FETCH_LOGIN_STORAGE, fetchloginStorage),
+  ]);
 }
