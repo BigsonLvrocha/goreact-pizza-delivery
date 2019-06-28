3174a53d51e9c95bab8749582139d87f371915e3
Luiz Victor Linhares Rocha <luizvictorlrocha@gmail.com>
2019-06-28 01:11:44 -0300

    adds saga to check admin token in session storage

--- src/store/sagas/session.js
+++ src/store/sagas/session.js
@@ -16,9 +16,32 @@ export function* loginRequest({ payload: { email, password } }) {
     if (user.role !== 'admin') {
       yield put(SessionActions.loginFailure('Permissão negada'));
     } else {
+      window.sessionStorage.setItem('ADMIN/adminToken', data.token);
       yield put(SessionActions.loginSuccess(user, data.token));
     }
   } catch (e) {
     yield put(SessionActions.loginFailure('Permissão negada'));
+  }
+}
+
+export function* fetchloginStorage() {
+  if (window.sessionStorage.getItem('ADMIN/adminToken')) {
+    const token = window.sessionStorage.getItem('ADMIN/adminToken');
+    try {
+      const { data: user } = yield call(Api.get, '/session', {
+        headers: {
+          Authorization: `bearer ${token}`,
+        },
+      });
+      if (user.role !== 'admin') {
+        yield put(SessionActions.loginFailure('Permissão negada'));
+      } else {
+        yield put(SessionActions.loginSuccess(user, token));
+      }
+    } catch (e) {
+      yield put(SessionActions.loginFailure('Permissão negada'));
+    }
+  } else {
+    yield put(SessionActions.loginFailure(false));
   }
 }
