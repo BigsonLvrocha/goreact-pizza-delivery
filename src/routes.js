1b752c42a0c7a7045ce6d8750921b6a011048653
Luiz Victor Linhares Rocha <luizvictorlrocha@gmail.com>
2019-06-27 22:47:07 -0300

    sets router to check if admin is logged

--- src/routes.js
+++ src/routes.js
@@ -2,17 +2,27 @@ import React from 'react';
 import {
   BrowserRouter, Route, Switch, Redirect,
 } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import Login from './pages/Login';
 import Orders from './pages/Orders';
 
-const Routes = () => (
+const Routes = ({ isUserLogged }) => (
   <BrowserRouter>
     <Switch>
-      <Route path="/" exact component={Login} />
-      <Route path="/order" exact component={Orders} />
+      <Route path="/" exact render={() => (isUserLogged ? <Redirect to="/order" /> : <Login />)} />
+      <Route path="/order" exact render={() => (isUserLogged ? <Orders /> : <Redirect to="/" />)} />
       <Route render={() => <Redirect to="/" />} />
     </Switch>
   </BrowserRouter>
 );
 
-export default Routes;
+Routes.propTypes = {
+  isUserLogged: PropTypes.bool.isRequired,
+};
+
+const mapStateToProps = state => ({
+  isUserLogged: state.session.adminData === null || state.session.adminToken === null,
+});
+
+export default connect(mapStateToProps)(Routes);
