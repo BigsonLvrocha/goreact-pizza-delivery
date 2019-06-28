e3876ff6cb486014fc49f488ee5c2af0eedf602d
Luiz Victor Linhares Rocha <luizvictorlrocha@gmail.com>
2019-06-27 23:59:32 -0300

    adds main layout to orders page

--- src/pages/Orders/index.js
+++ src/pages/Orders/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Layout from '../../components/Layouts/Main';
 
 // import { Container } from './styles';
 
 const Orders = () => (
-  <div>
+  <Layout>
     <h1>Hello from orders page</h1>
-  </div>
+  </Layout>
 );
 
 export default Orders;
