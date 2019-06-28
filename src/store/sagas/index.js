14b1942a231e6e27b90b36025fe64dbc31f16210
Luiz Victor Linhares Rocha <luizvictorlrocha@gmail.com>
2019-06-28 13:07:04 -0300

    adds order saga to root saga

--- src/store/sagas/index.js
+++ src/store/sagas/index.js
@@ -1,10 +1,13 @@
 import { all, takeLatest } from 'redux-saga/effects';
 import { loginRequest, fetchloginStorage } from './session';
+import { fetchOrdersRequest } from './order';
 import { Types as SesionTypes } from '../ducks/session';
+import { Types as OrderTypes } from '../ducks/order';
 
 export default function* rootSaga() {
   yield all([
     takeLatest(SesionTypes.LOGIN_REQUEST, loginRequest),
     takeLatest(SesionTypes.FETCH_LOGIN_STORAGE, fetchloginStorage),
+    takeLatest(OrderTypes.FETCH_ORDERS_REQUEST, fetchOrdersRequest),
   ]);
 }
